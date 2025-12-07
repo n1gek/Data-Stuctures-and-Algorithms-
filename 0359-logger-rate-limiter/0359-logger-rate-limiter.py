fc517adc2f102cbb609bbbb3cadea9db0d87a944
Tadiwanashe Nyamapfene <139914488+n1gek@users.noreply.github.com>
2025-12-06 22:29:37 -0600

    Time: 14 ms (37.67%), Space: 24.7 MB (95.04%) - LeetHub

--- 0359-logger-rate-limiter/0359-logger-rate-limiter.py
+++ 0359-logger-rate-limiter/0359-logger-rate-limiter.py
@@ -3,12 +3,14 @@ class Logger:
         self.tracker = {}  # message -> last printed timestamp
 
     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        if message in self.tracker:
-            last = self.tracker[message]
-        else:
-            last = None
-            
-        if last is None or timestamp - last >= 10:
+        if message not in self.tracker:
             self.tracker[message] = timestamp
             return True
-        return False
+        else:
+            time = self.tracker[message]
+            
+            if timestamp - time >= 10:
+                self.tracker[message] = timestamp
+                return True
+            return False
+            
