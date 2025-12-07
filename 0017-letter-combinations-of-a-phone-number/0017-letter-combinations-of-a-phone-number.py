b4858ed4c1053b9cd58d888d4d54cc085c37b858
Tadiwanashe Nyamapfene <139914488+n1gek@users.noreply.github.com>
2025-12-06 20:12:41 -0600

    Time: 0 ms (100%), Space: 17.8 MB (67.22%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,40 +1,35 @@
 from collections import deque
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if len(digits) == 0:
-            return []
-        # 8 digits
-        # 1 digit maps to 3 => 24
-        # create a map to represent the relationships
-        mapp = {'2': ["a", 'b', 'c'],
-                '3': ['d', 'e', 'f'],
-                '4': ['g', 'h', 'i'],
-                '5': ['j', 'k', 'l'],
-                '6': ['m', 'n', 'o'],
-                '7': ['p', 'q', 'r', 's'],
-                '8': ['t', 'u', 'v'],
-                '9': ['w', 'x', 'y', 'z']
-        }
-        
-        # use recursion to build the strings
-        res = []
-        def helper(path, index): # takes in what we are currently building and any constraints
-            if len(path) == len(digits):
+        # backtracking
+
+        nums = {
+        '2': ['a', 'b', 'c'],
+        '3': ['d', 'e', 'f'],
+        '4': ['g', 'h', 'i'],
+        '5': ['j', 'k', 'l'],
+        '6': ['m', 'n', 'o'],
+        '7': ['p', 'q', 'r', 's'],
+        '8': ['t', 'u', 'v'],
+        '9': ['w', 'x', 'y', 'z']
+    }
+
+        def helper(digits, path, res, index):
+            if index == len(digits):
                 res.append(path)
-                return 
+                return
             
-            digit = digits[index]
-            for char in mapp[digit]: # iteration over the letters
-                new_path = path + char
-                helper(new_path, index + 1)
+            letters = nums[digits[index]] 
             
-        helper("", 0)
+            for letter in letters:
+                new_path = path + letter
+                helper(digits, new_path, res, index + 1)
+        
+        res = []
+        helper(digits, "", res, 0)
 
         return res
-
-
-
-
+   
 
 
 
