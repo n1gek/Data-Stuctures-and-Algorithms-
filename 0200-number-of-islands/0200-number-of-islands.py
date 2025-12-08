e191a15dfaac13a20592ca3d39ba1f356c2fbe0a
Tadiwanashe Nyamapfene <139914488+n1gek@users.noreply.github.com>
2025-12-07 22:07:56 -0600

    Time: 297 ms (12.83%), Space: 29 MB (5.73%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,42 +1,40 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        # use a for loop to traverse the grid
+        # implement a helper function which uses breadth first to explore and mark the visited land area
+        # from the for, increement the counter everytime I come across unvisited land
+        # record the visited land in a seen set
 
         seen = set()
+        count = 0
 
-        def explore(row, col):
-            rowInbound = 0 <= row < rows
-            colInbound = 0 <= col < cols
+        rows = len(grid)
+        cols = len(grid[0])
 
-            if not rowInbound or not colInbound:
+        def helper(row, col):
+            if ((row, col)) in seen:
+                return
+            
+            if not 0 <= row < rows or not 0 <= col < cols:
                 return
             
             if grid[row][col] == "0":
                 return
             
-            pos = (row, col)
-            if pos in seen:
-                return
-            
-            seen.add(pos)
-            
-            explore(row + 1, col),
-            explore(row, col + 1),
-            explore(row - 1, col),
-            explore(row, col - 1)
+            seen.add((row, col))
+
+            helper(row + 1, col)
+            helper(row - 1, col)
+            helper(row, col + 1)
+            helper(row, col - 1)
 
         
-        rows = len(grid)
-        cols = len(grid[0])
-        count = 0
-
         for row in range(rows):
             for col in range(cols):
-                pos = (row, col)
-                if grid[row][col] == "1" and pos not in seen:
-                    explore(row, col)
+                if (row, col) not in seen and grid[row][col] == "1":
                     count += 1
+                    helper(row, col)
         
         return count
-                    
 
         
