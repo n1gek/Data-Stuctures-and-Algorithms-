390506d4bf764996bcb0dd07dd264d2f35ef7662
Tadiwanashe Nyamapfene <139914488+n1gek@users.noreply.github.com>
2025-12-07 21:20:01 -0600

    Time: 4637 ms (36.04%), Space: 18.1 MB (21.53%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -7,32 +7,29 @@ class Solution:
         def helper(row, col, index):
             if index == len(word):
                 return True
-
-            row_inbound = 0 <= row < rows
-            col_inbound = 0 <= col < cols
-
-            if not row_inbound or not col_inbound:
+            
+            if not 0 <= row < rows or not 0 <= col < cols:
                 return False
             if (row, col) in seen:
                 return False
             if board[row][col] != word[index]:
                 return False
-            
             seen.add((row, col))
-
-            res =( helper(row + 1, col, index + 1) or 
-            helper(row, col + 1, index + 1) or 
-            helper(row - 1, col, index + 1)or
-            helper(row, col - 1, index + 1))
             
+            res = (helper(row + 1, col, index + 1) or
+            helper(row - 1, col, index + 1) or
+            helper(row, col + 1, index + 1) or
+            helper(row, col - 1, index + 1))
+
             seen.remove((row, col))
 
             return res
         
         for row in range(rows):
             for col in range(cols):
-                if helper(row, col, 0):
-                    return True
+                if (row, col) not in seen:
+                    if helper(row, col, 0):
+                        return True
+        
         return False
 
-                    
