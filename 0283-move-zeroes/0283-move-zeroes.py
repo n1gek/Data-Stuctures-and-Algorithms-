379052da2758a63f6e85b0d2a54c44202044c4a6
Tadiwanashe Nyamapfene <139914488+n1gek@users.noreply.github.com>
2026-02-05 10:13:43 -0600

    Time: 6 ms (46.49%), Space: 20.4 MB (53.64%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -5,10 +5,8 @@ class Solution:
         """
         left = 0
 
-        for right in range(1, len(nums)):
-            if nums[left] != 0:
-                left += 1
-            if nums[left] == 0 and nums[right] != 0:
+        for right in range(len(nums)):
+            if nums[right] != 0:
                 nums[left], nums[right] = nums[right], nums[left]
                 left += 1
         
