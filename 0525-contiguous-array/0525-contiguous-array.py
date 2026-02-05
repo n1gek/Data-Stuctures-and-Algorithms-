9424102fddc8f3c9c89cf091c29489833afcf4ab
Tadiwanashe Nyamapfene <139914488+n1gek@users.noreply.github.com>
2026-02-05 14:33:00 -0600

    Time: 81 ms (69.01%), Space: 25.5 MB (18.59%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -1,30 +1,9 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        
-        # top_score = 0
 
-        # for x in range(len(nums)):
-        #     left = x
-        #     one_count = 0
-        #     zero_count = 0
-
-        #     while left < len(nums):
-        #         if nums[left] == 1:
-        #             one_count += 1
-        #         else:
-        #             zero_count += 1
-                
-        #         if one_count == zero_count:
-        #             length = left - x + 1
-        #             top_score = max(top_score, length)
-        #         left += 1
-                
-                
-        # return top_score
+        count = {0:-1}
         balance = 0
-        balance_map = {0:-1}
-        length = 0
-        #[1, 0]
+        res = 0
 
         for i, num in enumerate(nums):
             if num == 1:
@@ -32,18 +11,11 @@ class Solution:
             else:
                 balance -= 1
             
-            if balance in balance_map:
-                length = max(length, i - balance_map[balance])
+            if balance in count:
+                length = i - count[balance]
+                res = max(res, length)
             else:
-                balance_map[balance] = i
+                count[balance] = i 
         
-        return length
+        return res
 
-
-                    
-
-
-
-        
-
-        
