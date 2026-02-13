9477d351c930dd259ed313243ae6c7a2c28834f7
Tadiwanashe Nyamapfene <139914488+n1gek@users.noreply.github.com>
2026-02-13 09:55:25 -0600

    Time: 1371 ms (6.69%), Space: 70.8 MB (10.39%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -2,8 +2,9 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
 
         memo = {}
+        # dynamic programming
 
-        def helper(curr):
+        def helper(curr): # takes in the current amount
             if curr == 0:
                 return 0
             if curr < 0:
@@ -13,19 +14,20 @@ class Solution:
                 return memo[curr]
             
             best = float("inf")
-            
             for c in coins:
-                res = helper(curr - c)
+                res = helper(curr - c) # recurrence relation: we compute how many ways to build the smaller coin'
                 if res != float("inf"):
                     best = min(best, res + 1)
             
             memo[curr] = best
             return best
+        
+        result = helper(amount)
+
+        if result == float("inf"):
+            return -1
+        else:
+            return result
+
 
         
-        ans = helper(amount)
-
-        return ans if ans != float("inf") else -1
-
-            
-
