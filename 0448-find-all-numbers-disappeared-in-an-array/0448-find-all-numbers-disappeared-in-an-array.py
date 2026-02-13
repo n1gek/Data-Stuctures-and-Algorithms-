5b4b085b0c6a2835ee0398f4382fa66690d094ac
Tadiwanashe Nyamapfene <139914488+n1gek@users.noreply.github.com>
2026-02-13 09:36:09 -0600

    Time: 58 ms (8.29%), Space: 30.7 MB (55.79%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -1,6 +1,6 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        
+
         for num in nums:
             index = abs(num) - 1
             nums[index] = abs(nums[index]) * -1
@@ -8,9 +8,11 @@ class Solution:
         print(nums)
         res = []
 
-        for i, num in enumerate(nums):
-            if num > 0:
-                n = i + 1
-                res.append(n)
+        for i, n in enumerate(nums):
+            if n > 0:
+                num = i + 1
+                res.append(num)
         
         return res
+
+        
